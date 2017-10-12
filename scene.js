554b657e09207b3b23464697067ef8092782e806
Stephen Krewson <stephen.krewson@gmail.com>
2017-10-12 10:04:31 -0400

    ok time to run :)

---
+++ scene.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var img = new Image();
+
+img.src = "img/nethawk.png";
+
+img.onload = function(){
+	var pattern = context.createPattern(img, "repeat");
+	context.fillStyle = pattern;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+}
